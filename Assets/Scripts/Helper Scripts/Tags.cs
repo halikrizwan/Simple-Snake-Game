57cf19ebb93bd4953791f6ca252f4483539dc4d7
halikrizwan <47154130+halikrizwan@users.noreply.github.com>
2020-01-28 21:17:24 +0545

    Add files via upload

---
+++ Assets/Scripts/Helper Scripts/Tags.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tag{
+    public static string WALL="Wall";
+    public static string FRUIT="Fruit";
+    public static string TAIL="Tail";}
+
+public class Metrics{
+    public static float NODE = 0.2f;}
+
+public enum PlayerDirection{
+    LEFT = 0,
+    UP = 1,
+    RIGHT = 2,
+    DOWN = 3,
+    COUNT = 4 }
